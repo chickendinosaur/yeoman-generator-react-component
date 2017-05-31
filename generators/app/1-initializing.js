bbd1e7480cb80a9786b1efbae7542ef529dd7318
John Pittman <john@chickendinosaur.com>
2017-05-31 17:13:54 -0500

    Scripts.

--- generators/app/1-initializing.js
+++ generators/app/1-initializing.js
@@ -15,9 +15,9 @@ module.exports = function () {
       'babel-preset-es2015',
       'babel-preset-react',
       'babel-preset-stage-2',
-      "babelify",
-      "browser-run",
-      "browserify",
+      'babelify',
+      'browser-run',
+      'browserify',
       'csscomb',
       'css-loader',
       'file-loader',
@@ -36,10 +36,11 @@ module.exports = function () {
       'react-dom@*'
     ],
     scripts: {
-      'benchmark': "cd benchmark && for /R %i in (*.benchmark.*) do echo. && echo %i && echo. && browserify -t babelify %i | browser-run",
-      'build': 'npm run clean && babel src -d lib && cp package.json lib & cp README.md lib & cp LICENSE lib',
+      'benchmark': 'cd benchmark && for /R %i in (*.benchmark.*) do echo. && echo %i && echo. && browserify -t babelify %i | browser-run',
+      'build': 'npm run clean && babel src -d lib',
       'clean': 'rm -rf lib',
-      'deploy': 'npm run test && npm run build && npm publish lib',
+      'lint': 'eslint "src/**/*.{jsx,js}" --fix && eslint "stories/**/*.{jsx,js}" --fix && eslint "test/**/*.{jsx,js}" --fix && csscomb src',
+      "prepublish": "npm run test && npm run lint && npm run build",
       'storybook': 'npm run test && opn http://localhost:9999 && start-storybook -p 9999 -c .storybook',
       'test': 'babel-tape-runner test/**/*.test.* && eslint src --fix && eslint stories --fix && csscomb src'
   	}
